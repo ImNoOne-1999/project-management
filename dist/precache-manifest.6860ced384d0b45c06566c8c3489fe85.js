5851604d43e4a2ce38f3f33a530fa1221fd304ab
ImNoOne-1999 <konasr@rknec.edu>
2020-07-15 13:07:15 +0530

    done with hosting

---
+++ dist/precache-manifest.6860ced384d0b45c06566c8c3489fe85.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3723d3350dce2dd0ae8fccb647670612",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c2d12ffadf9ee8e20108",
+    "url": "/static/css/main.5c11289f.chunk.css"
+  },
+  {
+    "revision": "315a199287bddb45a31a",
+    "url": "/static/js/2.c30d997f.chunk.js"
+  },
+  {
+    "revision": "0135287500bd9d64203d07b4f33f971f",
+    "url": "/static/js/2.c30d997f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c2d12ffadf9ee8e20108",
+    "url": "/static/js/main.03cad202.chunk.js"
+  },
+  {
+    "revision": "14f6edad4b67c8d1e620",
+    "url": "/static/js/runtime-main.0f90b3a4.js"
+  }
+]);
